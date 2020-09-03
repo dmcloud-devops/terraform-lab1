890967a906c3b74368febaf27489ae1d39e8edff
Cristiano Diedrich <cristiano@umov.me>
2020-09-03 17:44:43 -0300

    Adiciona codigo de criaçao dos SG

---
+++ sg.tf
@@ -0,0 +1,71 @@
+resource "aws_security_group" "lab1_internal" {
+  name        = "lab1_internal"
+  description = "Allow SSH from specific ip address"
+  
+  ingress {
+    description = "Home"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["186.227.224.162/32"]
+  }
+
+  ingress {
+    description = "Local HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+
+  ingress {
+    description = "Local HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lab1_internal"
+  }
+}
+
+resource "aws_security_group" "lab1_external" {
+  name        = "lab1_external"
+  description = "Allow HTTP and HTTPS from internet"
+
+  ingress {
+    description = "External HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "External HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lab1_external"
+  }
+}
